090ba8ee26a2504d234105b168841b1ef12b06bb
Josec690 <jose.silva690@fatec.sp.gov.br>
2024-06-11 23:06:15 -0300

    Commit-15

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    development: {
-      username: root,
-      password: '',
-      database: projeto,
-      host: localhost,
-      dialect: 'mysql',
-      port: 3306
-    }
-  };
+  development: {
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'projeto',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'mysql',
+    port: process.env.DB_PORT || 3306
+  }
+};
   
